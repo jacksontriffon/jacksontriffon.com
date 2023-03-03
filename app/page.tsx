3abe2b2b9b2f0c671ceba3e5289c4742b72fe031
jacksontriffon <jackson.triffon@gmail.com>
2023-03-03 19:21:51 +1100

    Bare bones accordian components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,34 @@
 import styles from "./page.module.css";
-import Header from "../components/jt-header/jt-header";
-import Tabs from "../components/tabs/tabs";
+import Header from "@/components/jt-header/jt-header";
+import Tabs from "@/components/tabs/tabs";
+import Accordian, { AccordianDetails } from "@/components/accordian/accordian";
+import typescriptIcon from "../assets/logo/TypeScript.svg";
 
 export default function Home() {
+	const skillsList: [AccordianDetails] = [
+		{
+			title: "Full-stack",
+			description:
+				"Experienced in building: Websites, Mobile apps, and browser-extensions",
+			listDescription: "What I love building with:",
+			listItems: [
+				{
+					imageDetails: {
+						alt: "Typescript Logo",
+						src: typescriptIcon,
+					},
+					title: "TypeScript",
+				},
+			],
+		},
+	];
 	return (
 		<main className={styles.main}>
 			<Header></Header>
+			<div>
+				<a href="/contact">Jackson Triffon</a>
+				<Accordian {...skillsList[0]}></Accordian>
+			</div>
 			<Tabs></Tabs>
 		</main>
 	);
---
+++ components/accordian/accordian.tsx
@@ -0,0 +1,59 @@
+import styles from "./accordian.module.css";
+import Image, { ImageProps } from "next/image";
+
+export type AccordianDetails = {
+	title: string;
+	description: string;
+	listDescription: string;
+	listItems: [AccordianItemDetails];
+};
+
+export type AccordianItemDetails = {
+	imageDetails: ImageProps;
+	title: string;
+};
+
+export default function Accordian(details: AccordianDetails) {
+	return (
+		<div className={styles.accordianContainer}>
+			<div className={styles.leftColumn}>
+				<div className={styles.block}></div>
+				<div className={styles.thread}></div>
+			</div>
+			<div className={styles.rightColumn}>
+				<h1 className={styles.title}>{details.title}</h1>
+				<p className={styles.description}>{details.description}</p>
+				<span className={styles.listDescription}>
+					{details.listDescription}
+				</span>
+				<div className={styles.listContainer}>
+					<div className={styles.item}>
+						<Image
+							{...details.listItems[0].imageDetails}
+							alt={details.listItems[0].title + " Logo"}
+						></Image>
+						<p>{details.listItems[0].title}</p>
+					</div>
+					{details.listItems.length > 1 ? (
+						<div className={styles.item}>
+							<Image
+								{...details.listItems[1].imageDetails}
+								alt={details.listItems[1].title + " Logo"}
+							></Image>
+							<p>{details.listItems[1].title}</p>
+						</div>
+					) : null}
+					{details.listItems.length > 2 ? (
+						<div className={styles.item}>
+							<Image
+								{...details.listItems[2].imageDetails}
+								alt={details.listItems[2].title + " Logo"}
+							></Image>
+							<p>{details.listItems[2].title}</p>
+						</div>
+					) : null}
+				</div>
+			</div>
+		</div>
+	);
+}
